b8ec21f58b2be25c341305e85697dffd4b87224f
marxlima <marxmaciel@gmail.com>
2024-01-30 19:00:31 -0300

    criacao das tabelas coments post user

--- database/migrations/2024_01_30_182801_create_comments_table.php
+++ database/migrations/2024_01_30_182801_create_comments_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,13 +10,18 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('user_id');
             $table->string('title')->unique();
             $table->text('body');
             $table->timestamps();
 
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->ondelete('cascade');
             $table->foreign('user_id')
                 ->references('id')
                 ->on('users')
@@ -30,6 +34,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
